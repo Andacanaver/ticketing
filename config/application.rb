26898f71a8ad29ea57485bb3a7984cff6145efaa
Jacob M <moe.jacobw@gmail.com>
2022-06-07 14:56:02 -0500

    created Users controller

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Raillite.load
+Dotenv::Railtie.load
 
 # HOSTNAME = ENV['HOSTNAME']
 
